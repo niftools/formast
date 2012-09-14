be60f3b0d1a2fb7e0efef66f260e24e9ad08c90e
Amorilia <amorilia@users.sourceforge.net>
2012-09-14 23:18:15 +0100

    Initial documentation, and further enhancements to public API.

--- include/formast.hpp
+++ include/formast.hpp
@@ -8,14 +8,15 @@
 namespace formast
 {
 
+//! Expression node of the abstract syntax tree.
 class Expr
 {
 public:
-    template<typename iterator_type>
-    Expr(iterator_type iter, iterator_type end);
+    template<typename Iterator>
+    bool parse_xml(Iterator & iter, Iterator end);
 };
 
-
+//! Visitor for the abstract syntax tree.
 template <typename type>
 class Visitor
 {
@@ -33,5 +34,38 @@ public:
 };
 
 } // namespace formast
+
+/*! \mainpage FormAST Documentation
+
+FormAST stands for *file format abstract syntax tree*.
+It's primary purpose is to expose the abstract syntax tree
+of a file format description.
+
+\section Visitation
+
+Rather than exposing the data structure
+representing the abstract syntax tree directly,
+FormAST exposes the structure by means of
+<a href="http://en.wikipedia.org/wiki/Visitor_pattern">visitation</a>.
+In practice, visitation provides every means for evaluating
+the abstract syntax tree,
+or for compiling it into another tree structure
+for whatever sofisticated purpose.
+Moreover, the visitor model allows new language features to be added
+with minimal change to the public API.
+
+Simply derive a subclass from formast::Visit
+and override the desired methods.
+
+\section Expressions
+
+For the time being, FormAST exposes just a single node type,
+namely formast::Expr,
+for expressions.
+
+Expressions are created from xml code,
+via formast::Expr::parse_xml().
+
+*/
 
 #endif
