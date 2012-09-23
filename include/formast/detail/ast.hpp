9b1b6856bb38d89fdaa6053d5578073a1f4f6c86
Amorilia <amorilia@users.sourceforge.net>
2012-09-23 18:36:00 +0100

    Define class, attribute, if-elifs-else, and related nodes, of the ast.

--- include/formast/detail/ast.hpp
+++ include/formast/detail/ast.hpp
@@ -1,8 +1,13 @@
 #ifndef FORMAST_DETAIL_AST_HPP_INCLUDED
 #define FORMAST_DETAIL_AST_HPP_INCLUDED
 
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/optional.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/variant/recursive_variant.hpp>
+#include <list>
+#include <string>
+#include <vector>
 
 // this header file provides an implementation of the abstract syntax tree
 
@@ -12,6 +17,42 @@ namespace detail
 {
 namespace ast
 {
+
+// documentation node
+
+typedef std::list<std::string> Doc;
+
+// declaration node
+
+struct Attr;
+struct Class;
+struct IfElifsElse;
+typedef boost::make_recursive_variant<Class, Attr, IfElifsElse>::type Declaration;
+
+// scope node
+
+typedef std::vector<Declaration> Scope;
+
+// attribute node
+
+struct Attr
+{
+    std::string class_name;
+    std::string name;
+    boost::optional<Doc> doc;
+};
+
+// class node
+
+struct Class
+{
+    std::string name;
+    boost::optional<std::string> base_name;
+    boost::optional<Doc> doc;
+    boost::optional<Scope> scope;
+};
+
+// expression nodes
 
 struct binary_op;
 struct unary_op;
@@ -41,8 +82,51 @@ struct unary_op {
     Expr right;
 };
 
+// conditional nodes
+
+struct If
+{
+    Expr expr;
+    Scope scope;
+};
+
+class IfElifsElse
+{
+    std::vector<If> ifs_;
+    boost::optional<Scope> else_;
+};
+
 }
 }
 }
+
+// upgrade structs to fusion sequences
+
+BOOST_FUSION_ADAPT_STRUCT(
+    formast::detail::ast::Attr,
+    (std::string, class_name)
+    (std::string, name)
+    (boost::optional<formast::detail::ast::Doc>, doc)
+)
+
+BOOST_FUSION_ADAPT_STRUCT(
+    formast::detail::ast::Class,
+    (std::string, name)
+    (boost::optional<std::string>, base_name)
+    (boost::optional<formast::detail::ast::Doc>, doc)
+    (boost::optional<formast::detail::ast::Scope>, scope)
+)
+
+BOOST_FUSION_ADAPT_STRUCT(
+    formast::detail::ast::If,
+    (formast::detail::ast::Expr, expr)
+    (formast::detail::ast::Scope, scope)
+)
+
+BOOST_FUSION_ADAPT_STRUCT(
+    formast::detail::ast::IfElifsElse,
+    (std::vector<formast::detail::ast::If>, ifs_)
+    (boost::optional<formast::detail::ast::Scope>, else_)
+)
 
 #endif
