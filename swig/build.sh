9b598658c003265f132a432872cf57aba523a51f
Amorilia <amorilia@users.sourceforge.net>
2012-09-23 08:48:51 +0100

    Moved python test files.

--- swig/build.sh
+++
@@ -1,8 +0,0 @@
-# where we can find the built formast.so C++ library
-BUILDPATH=../../../build/formast/
-
-swig -c++ -python -I../include/ formast.i
-g++ -fPIC -c formast_wrap.cxx -o formast_wrap.o -I/usr/include/python2.7 -I../include/
-g++ -shared formast_wrap.o -L$BUILDPATH -lformast -o _formast.so
-LD_LIBRARY_PATH=$BUILDPATH python test.py
-
