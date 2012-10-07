8b764ff32bbd12cef5be4d7101718878155d4ca9
Amorilia <amorilia@users.sourceforge.net>
2012-10-07 12:04:05 +0100

    Initial runtime test framework, and some bugs fixed.

--- swig/python/package/test/runtime/__init__.py
+++ swig/python/package/test/runtime/__init__.py
@@ -5,9 +5,13 @@
 # a realistic example would implement all operators
 
 import formast
+import struct
 
 def api_name(name):
     return "_".join(part.lower() for part in name.split())
+
+def read_int(stream):
+    return struct.unpack('<i', stream.read(4))[0]
 
 class RuntimeExprEval(formast.Visitor):
     """Generate code for expression."""
@@ -51,8 +55,8 @@ class RuntimeClass(object):
     def __init__(self):
         RuntimeClassInit(self)
 
-    def read(self):
-        RuntimeClassRead(self)
+    def read(self, stream):
+        RuntimeClassRead(self, stream)
 
 class RuntimeClassInit(formast.Visitor):
     """Construct an instance by visitation of the class AST."""
@@ -77,39 +81,52 @@ class RuntimeClassInit(formast.Visitor):
 class RuntimeClassRead(formast.Visitor):
     """Read an instance by visitation of the class AST."""
 
-    def __init__(self, obj):
+    def __init__(self, obj, stream):
         formast.Visitor.__init__(self)
         self.obj = obj
+        self.stream = stream
         if obj._ast_class.stats.is_initialized():
             self.stats(obj._ast_class.stats.get())
 
     def stats_attr(self, a):
         # everything is an integer, so this is rather simple
         if not a.arr1.is_initialized():
-            setattr(self.obj, api_name(a.name), read_int(stream))
+            setattr(self.obj, api_name(a.name), read_int(self.stream))
         else:
-            expr_eval = RuntimeExprEval()
+            expr_eval = RuntimeExprEval(self)
             expr_eval.expr(a.arr1.get())
             setattr(self.obj, api_name(a.name),
-                    [read_int(stream) for i in range(expr_eval.stack.pop())])
+                    [read_int(self.stream)
+                     for i in range(expr_eval.stack.pop())])
 
     def stats_if(self, if_):
-        expr_eval = RuntimeExprEval()
+        expr_eval = RuntimeExprEval(self)
         expr_eval.expr(if_.expr)
         if expr_eval.stack.pop():
             self.stats(if_.then)
         # if_.else_ not used
 
-class RuntimeModule(formast.Visitor):
-    """Generate module-like object."""
+class RuntimeModule(object):
+    """Module-like object."""
+
+    def __init__(self, top):
+        self._ast_top = top
+        RuntimeModuleInit(self)
+
+class RuntimeModuleInit(formast.Visitor):
+    """Populate module with classes."""
+
+    def __init__(self, mod):
+        formast.Visitor.__init__(self)
+        self.mod = mod
+        self.top(mod._ast_top)
 
     def top_class(self, c):
-        setattr(self, api_name(c.name),
+        setattr(self.mod, c.name,
                 type(c.name, (RuntimeClass,), dict(_ast_class=c)))
 
 if __name__ == "__main__":
     top = formast.Top()
     with open("../codegen/integers.xml", "rb") as stream:
         formast.XmlParser().parse_string(stream.read(), top)
-    mod = RuntimeModule()
-    mod.top(top)
+    mod = RuntimeModule(top)
