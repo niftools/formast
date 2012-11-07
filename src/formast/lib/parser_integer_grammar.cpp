3227a296740b4088f02e5a5e5777872c01eaeb91
Amorilia <amorilia@users.sourceforge.net>
2012-11-07 18:18:32 +0000

    Fix hex parsing.

--- src/formast/lib/parser_integer_grammar.cpp
+++ src/formast/lib/parser_integer_grammar.cpp
@@ -23,7 +23,7 @@ formast::Parser::Impl::integer_grammar::integer_grammar() : integer_grammar::bas
     qi::_4_type _4;
 
     uint_or_version =
-        hex
+        (hex              [_val = _1])
         |   (lit("2.3")   [_val = 0x02030000])
         |   (lit("3.03")  [_val = 0x03000300]) // special case
         |   (lit("3.0")   [_val = 0x03000000])
@@ -34,10 +34,11 @@ formast::Parser::Impl::integer_grammar::integer_grammar() : integer_grammar::bas
                   > '.' > (ulong_long   [_val = _val * 256 + _1])))
         ;
 
-    hex = lit("0x") > hex_parser;
+    hex %= lit("0x") > hex_parser;
 
     // Debugging and error handling and reporting support.
     BOOST_SPIRIT_DEBUG_NODE(uint_or_version);
+    BOOST_SPIRIT_DEBUG_NODE(hex);
 
     // Error handling
     qi::on_error<qi::fail>(uint_or_version, error_handler(_4, _3, _2));
